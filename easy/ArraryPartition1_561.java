7a9e55715dda17adb7c949c83b16f70a11b3f88a
William Borden <46210325+wborden@users.noreply.github.com>
2019-04-12 22:43:05 -0500

    Leetcode 561
    Added leetcode 561 Array Partition 1 Java solution

---
+++ easy/ArraryPartition1_561.java
@@ -0,0 +1,14 @@
+//Leetcode problem Array Partition 1 #561 
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int sum = 0;
+		int i = 0;
+        while(i < nums.length) {
+            sum += nums[i];
+            i += 2;
+        }
+        return sum;
+    }
+}
